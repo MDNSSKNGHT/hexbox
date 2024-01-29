1969d0fafb59d35bbee8a92ca9994dfe6fb6f7a2
Leonardo Estacio <mdnssknght@gmail.com>
2024-01-29 17:16:19 -0500

    hexbox: describe the hex view as structure
    Signed-off-by: Leonardo Estacio <mdnssknght@gmail.com>

--- hexbox.c
+++ hexbox.c
@@ -1,4 +1,11 @@
+//
+// Made by Leonardo in Jan 29th of 2024
+// Have any questions or inquiries? Contact me: mdnssknght at tuta dot io
+// Happy hacking!
+//
+
 #define TB_IMPL
+
 #include "termbox.h"
 #include <stdbool.h>
 #include <stdint.h>
@@ -11,6 +18,7 @@
 //
 #define BUFFER_SIZE 1024 * 1024
 
+#define BYTE_ZERO "00"
 #define OFFSET_ZERO "0x00000000"
 
 #define LUCORNER 0x250c
@@ -32,15 +40,20 @@ struct cursor_pos {
 	int x, y;
 };
 
-/* struct hex_view { */
-/* 	struct box drawing_area; */
-/* 	struct box offsets_drawing_area; */
-/* 	struct box hexdump_drawing_area; */
-/* 	struct box strings_drawing_area; */
-/* }; */
+struct view {
+	struct box draw_area;
+};
 
-/* struct hex_view hexview_drawing_area; */
+struct hex_view {
+	struct view root;
+	struct view offsets;
+	struct view hexdump;
+	struct view strings;
+};
 
+//
+// draws a simple box, pretty eh?
+//
 static void draw_box(void) {
 	int w = tb_width();
 	int h = tb_height();
@@ -67,32 +80,41 @@ static void draw_box(void) {
 //
 // <offset>: <hex dump at offset>
 //
-static void hex_view(uint8_t *buff, uint8_t start, struct box hexview_drawing_area) {
-	struct box hexdump_drawing_area = hexview_drawing_area;
-	hexdump_drawing_area.x += strlen(OFFSET_ZERO) + 1;
-	hexdump_drawing_area.w -= (hexview_drawing_area.x + hexdump_drawing_area.x) - 1;
+// draws the Hex view containing the offset and hex dump
+//
+static void hex_view(struct hex_view *hexview, uint8_t *buff, uint8_t start) {
+	hexview->hexdump.draw_area = hexview->root.draw_area;
+	hexview->hexdump.draw_area.x += strlen(OFFSET_ZERO) + 1;
+	hexview->hexdump.draw_area.w -= (hexview->root.draw_area.x + hexview->hexdump.draw_area.x) - 1;
 
-	int nsegments = hexdump_drawing_area.w / 3;
+	int nsegments = hexview->hexdump.draw_area.w / strlen(BYTE_ZERO " ");
 
-	for (int i = 0; i < hexdump_drawing_area.h - 1; i++) {
+	for (int i = 0; i < hexview->hexdump.draw_area.h - 1; i++) {
 		for (int j = 0; j < nsegments; j++) {
 			uint8_t byte = buff[(i + start) * nsegments + j];
-			tb_printf(hexdump_drawing_area.x + (j * 3), hexdump_drawing_area.y + i, 0, 0, "%.2X", byte);
+			tb_printf(hexview->hexdump.draw_area.x + (j * 3), hexview->hexdump.draw_area.y + i,
+					0, 0, "%.2X", byte);
 		}
 	}
 
-	struct box offset_drawing_area = hexview_drawing_area;
+	hexview->offsets.draw_area = hexview->root.draw_area;
 
-	for (int i = 0; i < offset_drawing_area.h - 1; i++) {
-		tb_printf(offset_drawing_area.x, offset_drawing_area.y + i, 0, 0, "0x%08x", (i + start) * nsegments);
+	for (int i = 0; i < hexview->offsets.draw_area.h - 1; i++) {
+		tb_printf(hexview->offsets.draw_area.x, hexview->offsets.draw_area.y + i,
+				0, 0, "0x%08x", (i + start) * nsegments);
 	}
 
 	tb_present();
 }
 
-static bool cursor_pos_clamp(struct cursor_pos *cpos, struct box box) {
-	int x = cpos->x < box.x ? box.x : MIN(cpos->x, box.x + box.w);
-	int y = cpos->y < box.y ? box.y : MIN(cpos->y, box.y + box.h);
+static bool cursor_pos_clamp(struct cursor_pos *cpos, struct box limit) {
+	// we do cpos->{x,y} - limit.{x,y} to normalize the coords
+	// i.e if the cursor origin is located at (4, 3) in the terminal buffer
+	// and the cursor position is located at (5, 6) in the terminal buffer
+	// we subtract the origin cords from the position to get the new position
+	// coords with an origin at (0, 0).
+	int x = (cpos->x - limit.x) < 0 ? limit.x : MIN(cpos->x, limit.x + limit.w);
+	int y = (cpos->y - limit.y) < 0 ? limit.y : MIN(cpos->y, limit.y + limit.h);
 
 	if (cpos->x != x || cpos->y != y) {
 		cpos->x = x;
@@ -122,24 +144,24 @@ int main(int argc, char *argv[]) {
 	fread(buff, sizeof(uint8_t), BUFFER_SIZE, f);
 	fclose(f);
 
-	struct box hexview_drawing_area = {0};
-	hexview_drawing_area.x = 1;
-	hexview_drawing_area.y = 1;
-	hexview_drawing_area.w = tb_width() - hexview_drawing_area.x;
-	hexview_drawing_area.h = tb_height() - hexview_drawing_area.y;
+	struct hex_view hexview = {0};
+
+	hexview.root.draw_area.x = 1;
+	hexview.root.draw_area.y = 1;
+	hexview.root.draw_area.w = tb_width() - hexview.root.draw_area.x;
+	hexview.root.draw_area.h = tb_height() - hexview.root.draw_area.y;
 
 	int start = 0;
 
-	hex_view(buff, start, hexview_drawing_area);
+	hex_view(&hexview, buff, start);
 
-	struct box hexdump_drawing_area = hexview_drawing_area;
-	hexdump_drawing_area.x += strlen(OFFSET_ZERO) + 1;
-	hexdump_drawing_area.w -= (hexview_drawing_area.x + hexdump_drawing_area.x) + 1;
-	hexdump_drawing_area.h -= hexdump_drawing_area.y + 1;
+	struct box cursor_limits = hexview.hexdump.draw_area;
+	cursor_limits.w -= hexview.root.draw_area.x + 1;
+	cursor_limits.h -= hexview.hexdump.draw_area.y + 1;
 
 	struct cursor_pos cpos = {0};
-	cpos.x = hexdump_drawing_area.x;
-	cpos.y = hexdump_drawing_area.y;
+	cpos.x = hexview.hexdump.draw_area.x;
+	cpos.y = hexview.hexdump.draw_area.y;
 	cursor_pos_update(cpos);
 
 	struct tb_event ev;
@@ -155,28 +177,28 @@ int main(int argc, char *argv[]) {
 		switch (ev.key) {
 			case TB_KEY_ARROW_UP:
 				cpos.y--;
-				if (cursor_pos_clamp(&cpos, hexdump_drawing_area)) {
+				if (cursor_pos_clamp(&cpos, cursor_limits)) {
 					start = MAX(0, start - 1);
-					hex_view(buff, start, hexview_drawing_area);
+					hex_view(&hexview, buff, start);
 				}
 				cursor_pos_update(cpos);
 				break;
 			case TB_KEY_ARROW_DOWN:
 				cpos.y++;
-				if (cursor_pos_clamp(&cpos, hexdump_drawing_area)) {
+				if (cursor_pos_clamp(&cpos, cursor_limits)) {
 					start++;
-					hex_view(buff, start, hexview_drawing_area);
+					hex_view(&hexview, buff, start);
 				}
 				cursor_pos_update(cpos);
 				break;
 			case TB_KEY_ARROW_LEFT:
 				cpos.x--;
-				cursor_pos_clamp(&cpos, hexdump_drawing_area);
+				cursor_pos_clamp(&cpos, cursor_limits);
 				cursor_pos_update(cpos);
 				break;
 			case TB_KEY_ARROW_RIGHT:
 				cpos.x++;
-				cursor_pos_clamp(&cpos, hexdump_drawing_area);
+				cursor_pos_clamp(&cpos, cursor_limits);
 				cursor_pos_update(cpos);
 				break;
 			case TB_KEY_ESC:
